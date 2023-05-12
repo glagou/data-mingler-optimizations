75b4738a3f118d64e915d9f1c5f716ebbed35634
sotirangelo <sotiraggelos@gmail.com>
2023-05-12 16:39:39 +0300

    Refactor RollupOperator with new Graph types

--- src/main/java/gr/aueb/data_mingler_optimizations/operator/RollupOperator.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/operator/RollupOperator.java
@@ -1,14 +1,27 @@
 package gr.aueb.data_mingler_optimizations.operator;
 
+import gr.aueb.data_mingler_optimizations.exception.InvalidNumberOfCmdArgumentsException;
 import gr.aueb.data_mingler_optimizations.util.GraphUtils;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
 
 public class RollupOperator {
 
-    public static void main(String[] args) {
+    private static void validateCmdArguments(String[] args) {
+        if (args.length != 3) {
+            throw new InvalidNumberOfCmdArgumentsException();
+        }
+    }
+
+    public static void main(String[] args) throws InvalidNumberOfCmdArgumentsException {
+        validateCmdArguments(args);
+
         String rootNode = args[0];
         String childNode = args[1];
         String childChildNode = args[2];
@@ -18,12 +31,12 @@ public class RollupOperator {
         String edge1 = rootNode + "-" + childNode;
         String edge2 = childNode + "-" + childChildNode;
 
-        Set<String> keys = GraphUtils.getElements(edge1);
+        Set<String> keys = new HashSet<String>(GraphUtils.getElements(edge1));
         for (String key : keys) {
-            Set<String> values = GraphUtils.getElements(edge1 + ":" + key);
+            List<String> values = new ArrayList<String>(GraphUtils.getElements(edge1 + ":" + key));
             GraphUtils.removeElement(edge1 + ":" + key);
             for (String value : values) {
-                Set<String> values2 = GraphUtils.getElements(edge2 + ":" + value);
+                List<String> values2 = new ArrayList<String>(GraphUtils.getElements(edge2 + ":" + value));
                 GraphUtils.putValue(edge1 + ":" + key, values2);
             }
         }
