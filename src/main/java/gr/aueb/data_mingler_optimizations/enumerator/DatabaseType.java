bc6ae745c968f2d423a86efd4e8e676499a56b97
sotirangelo <sotiraggelos@gmail.com>
2023-05-19 17:53:42 +0300

    Create enums for EdgesLoader

---
+++ src/main/java/gr/aueb/data_mingler_optimizations/enumerator/DatabaseType.java
@@ -0,0 +1,11 @@
+
+package gr.aueb.data_mingler_optimizations.enumerator;
+
+public enum DatabaseType {
+    MSACCESS,
+    ORACLE,
+    MYSQL,
+    SQLSERVER,
+    POSTGRES,
+    DB2
+}
---
+++ src/main/java/gr/aueb/data_mingler_optimizations/enumerator/SourceType.java
@@ -0,0 +1,8 @@
+package gr.aueb.data_mingler_optimizations.enumerator;
+
+public enum SourceType {
+    DB,
+    CSV,
+    EXCEL,
+    PROCESS,
+}
