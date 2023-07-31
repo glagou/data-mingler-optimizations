d6c0d11a19300ab2eba6b85c2c37f2d402652866
sotirangelo <sotiraggelos@gmail.com>
2023-07-31 21:10:43 +0300

    Complete initial ThetaCombineOperator

--- src/main/java/gr/aueb/data_mingler_optimizations/operator/ThetaCombineOperator.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/operator/ThetaCombineOperator.java
@@ -1,9 +1,6 @@
 package gr.aueb.data_mingler_optimizations.operator;
 
-import gr.aueb.data_mingler_optimizations.enumerator.AggregationType;
-import gr.aueb.data_mingler_optimizations.enumerator.KeyMode;
 import gr.aueb.data_mingler_optimizations.enumerator.StringConstant;
-import gr.aueb.data_mingler_optimizations.exception.InvalidNumberOfCmdArgumentsException;
 import gr.aueb.data_mingler_optimizations.util.GraphUtils;
 
 import javax.script.*;
@@ -16,40 +13,58 @@ public class ThetaCombineOperator {
     private static final ScriptEngine engine = manager.getEngineByName("python");
 
     public static void run(String rootNode, String newChildNode, String allChildNodesCL, String outputChildNodesCL,
-                            String thetaCL, KeyMode keyMode) throws InvalidNumberOfCmdArgumentsException {
+                            String thetaCL) throws ScriptException {
 
         Instant start = Instant.now();
+
         boolean hasOutput = !outputChildNodesCL.isEmpty();
         String[] allChildNodes = allChildNodesCL.split(StringConstant.COMMA.getValue());
         String[] outputChildNodes = outputChildNodesCL.split(StringConstant.COMMA.getValue());
 
-        String theta;
+        String theta = "True";
         if (!thetaCL.isEmpty()) {
             theta = thetaCL.replace('$'+rootNode+'$', "key");
             for (String childNode : allChildNodes) {
+                // TODO: Fix by adding "key" binding somehow. Originally was: "r.lrange(\""+rootNode+'-'+childNode+":\"+key,0,1)[0]"
                 theta = theta.replace(
-                        '$'+childNode+'$', "r.lrange(\""+rootNode+'-'+childNode+":\"+key,0,1)[0]"
+                        '$'+childNode+'$', ((List<String>) GraphUtils.getElements(rootNode+'-'+childNode)).get(0)
                 );
             }
         }
 
-        // TODO: Improve this
         boolean isFirst = true;
         Set<String> keys = new HashSet<>();
         for (String childNode : allChildNodes) {
             String edge = rootNode + "-" + childNode;
             if  (isFirst) {
-
                 keys = (Set<String>) GraphUtils.getElements(edge);
                 isFirst = false;
             }
-            keys.retainAll((Set<String>) GraphUtils.getElements(edge));
+            keys.retainAll(GraphUtils.getElements(edge));
         }
 
         String newEdge = rootNode + '-' + newChildNode;
 
         for (String key : keys) {
-            // TODO: eval theta
+            try {
+                Bindings bindings = new SimpleBindings();
+                bindings.put("key", key);
+                Object result = engine.eval(theta, bindings);
+                if (result instanceof Boolean && (Boolean) result) {
+                    GraphUtils.addValueToCollection(newEdge, key);
+                    if (hasOutput) {
+                        for (String childNode : outputChildNodes) {
+                            String nextEdge = rootNode + '-' + childNode + ':' + key;
+                            List<String> values = new ArrayList<>(GraphUtils.getElements(nextEdge));
+                            GraphUtils.putValue(newEdge+':'+key,values);
+                        }
+                    } else {
+                        GraphUtils.addValueToCollection(newEdge+':'+key,key);
+                    }
+                }
+            } catch (ScriptException e) {
+            System.out.println(e.getMessage());
+        }
         }
 
         Instant finish = Instant.now();
