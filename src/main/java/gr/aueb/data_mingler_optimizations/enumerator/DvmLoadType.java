1df5c73ff8316111afdfb9769e91c9b227c71416
glagou <g.lagoudakhs@gmail.com>
2023-08-02 00:13:46 +0300

    Improve quality and delete unused code

--- src/main/java/gr/aueb/data_mingler_optimizations/enumerator/DvmLoadType.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/enumerator/DvmLoadType.java
@@ -2,5 +2,6 @@ package gr.aueb.data_mingler_optimizations.enumerator;
 
 public enum DvmLoadType {
 
-    ADD, CLEAR
+    ADD,
+    CLEAR
 }
--- src/main/java/gr/aueb/data_mingler_optimizations/exception/UnableToInitializeDocumentAndXpathException.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/exception/UnableToInitializeDocumentAndXpathException.java
@@ -2,10 +2,10 @@ package gr.aueb.data_mingler_optimizations.exception;
 
 public class UnableToInitializeDocumentAndXpathException extends RuntimeException {
 
-    private static final String MESSAGE = "Document and Xpath could not be initialized with queryFilename: %s";
+    private static final String MESSAGE = "Document and Xpath could not be initialized for file name: %s";
 
-    public UnableToInitializeDocumentAndXpathException(String queryFilename) {
-        super(String.format(MESSAGE, queryFilename));
+    public UnableToInitializeDocumentAndXpathException(String fileName) {
+        super(String.format(MESSAGE, fileName));
     }
 
 }
