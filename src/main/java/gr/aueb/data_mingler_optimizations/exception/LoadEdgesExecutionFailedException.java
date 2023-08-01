920ed1bf80dba5d4c14fe0257dd28f4c498e01b0
glagou <g.lagoudakhs@gmail.com>
2023-08-02 00:06:05 +0300

    Improve exception message

--- src/main/java/gr/aueb/data_mingler_optimizations/exception/LoadEdgesExecutionFailedException.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/exception/LoadEdgesExecutionFailedException.java
@@ -2,7 +2,7 @@ package gr.aueb.data_mingler_optimizations.exception;
 
 public class LoadEdgesExecutionFailedException extends RuntimeException {
 
-    private static final String MESSAGE = "Load edges execution failed for root node: %s";
+    private static final String MESSAGE = "EdgesLoader program execution failed for root node: %s";
 
     public LoadEdgesExecutionFailedException(String rootNode, Exception e) {
         super(String.format(MESSAGE, rootNode), e);
