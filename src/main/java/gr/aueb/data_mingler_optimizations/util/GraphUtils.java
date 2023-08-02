5d3845ede1aaa868f1b489e727c493fa800d9090
glagou <g.lagoudakhs@gmail.com>
2023-08-02 20:28:53 +0300

    Refactor GraphUtils

--- src/main/java/gr/aueb/data_mingler_optimizations/util/GraphUtils.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/util/GraphUtils.java
@@ -3,7 +3,6 @@ package gr.aueb.data_mingler_optimizations.util;
 import gr.aueb.data_mingler_optimizations.QueryEvaluation;
 import gr.aueb.data_mingler_optimizations.enumerator.GraphAdditionMethod;
 import gr.aueb.data_mingler_optimizations.enumerator.KeysMode;
-import gr.aueb.data_mingler_optimizations.enumerator.StringConstant;
 import gr.aueb.data_mingler_optimizations.singleton.GraphManagerSingleton;
 
 import java.util.*;
@@ -12,52 +11,27 @@ public class GraphUtils {
 
     private static final Map<String, Collection<String>> GRAPH = GraphManagerSingleton.getGraph();
 
-    private static String createGraphKeyWithHyphen(String rootNode, String childNode) {
-        return rootNode.concat(StringConstant.HYPHEN.getValue()).concat(childNode);
-    }
-
-    public static void removeEdge(String rootNode, String childNode) {
-        String baseKey = createGraphKeyWithHyphen(rootNode, childNode);
-        Collection<String> keys = GRAPH.get(baseKey);
-        keys.forEach(key -> {
-            String childKey = baseKey
-                    .concat(StringConstant.COMMA.getValue())
-                    .concat(key);
-            GRAPH.remove(childKey);
-        });
-        GRAPH.remove(baseKey);
-    }
-
     public static Collection<String> combineKeys(String rootNode, List<String> childNodes) {
-        String graphKey = rootNode + "-" + childNodes.get(0);
-        Set<String> keys = (Set<String>) GRAPH.get(graphKey);
-        childNodes.forEach(childNode -> {
-            String childNodeGraphKey = rootNode + "-" + childNode;
-            if (QueryEvaluation.getKeysMode() == KeysMode.ALL) {
-                keys.addAll(GRAPH.get(childNodeGraphKey));
-            } else {
-                keys.retainAll(GRAPH.get(childNodeGraphKey));
+        Set<String> keys = null;
+        boolean isFirst = true;
+        for (String childNode : childNodes) {
+            String graphKey = rootNode + "-" + childNode;
+            if (isFirst) {
+                keys = (Set<String>) GraphUtils.getElements(graphKey);
+                isFirst = false;
+                continue;
             }
-        });
+            if (QueryEvaluation.getKeysMode() == KeysMode.ALL)
+                keys.addAll(GRAPH.get(graphKey));
+            else {
+                keys.retainAll(GRAPH.get(graphKey));
+            }
+        }
         return keys;
     }
 
     public static Collection<String> getElements(String edge) {
         return GRAPH.get(edge);
-    }
-
-    public static void setCollection(String key, Collection<String> values) {
-        GRAPH.put(key, values);
-    }
-
-    public static void addAll(String key, Collection<String> values) {
-        Collection<String> initialCollection = GRAPH.get(key);
-        if (initialCollection == null) {
-            setCollection(key, values);
-            return;
-        }
-        initialCollection.addAll(values);
-        setCollection(key, initialCollection);
     }
 
     public static void addValueToCollection(String key, String value, GraphAdditionMethod graphAdditionMethod) {
