4b05a28188a27729c80ca1c37b963c1806ead592
sotirangelo <sotiraggelos@gmail.com>
2023-09-20 22:44:28 +0300

    Integrate jep to MapOperator

--- src/main/java/gr/aueb/data_mingler_optimizations/operator/MapOperator.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/operator/MapOperator.java
@@ -1,35 +1,71 @@
 package gr.aueb.data_mingler_optimizations.operator;
 
 import gr.aueb.data_mingler_optimizations.enumerator.GraphAdditionMethod;
+import gr.aueb.data_mingler_optimizations.util.PythonUtils;
 import gr.aueb.data_mingler_optimizations.util.GraphUtils;
+import jep.Interpreter;
 import jep.JepException;
 import jep.SharedInterpreter;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MapOperator {
-    static SharedInterpreter interpreter = new SharedInterpreter();
 
-    public static void run(String rootNode, String childNode, String functionInvocation) {
+    static final Interpreter interpreter = new SharedInterpreter();
+    static final Pattern importPattern = Pattern.compile("^import .*|^from .* import .*");
+
+
+    public static void run(String rootNode, String childNode, String script) {
+        String pythonCode = getPythonCode(script).replace('$' + childNode + '$', "value");
+
         String edge = rootNode + "-" + childNode;
-        functionInvocation = functionInvocation.replace('$' + childNode + '$', "value");
         Set<String> keys = (Set<String>) GraphUtils.getElements(edge);
         for (String key : keys) {
             String graphKey = edge + ":" + key;
             Collection<String> values = GraphUtils.getElements(graphKey);
             GraphUtils.removeElement(graphKey);
-
-            for (String value : values) {
-                try {
+            try {
+                for (String value : values) {
+                    Object result = null;
                     interpreter.set("value", value);
-                    Object result = interpreter.getValue(functionInvocation);
+                    result = PythonUtils.getValueFromScript(pythonCode, interpreter);
                     GraphUtils.addValueToCollection(graphKey, String.valueOf(result), GraphAdditionMethod.AS_LIST);
-                } catch (JepException e) {
-                    throw new RuntimeException(e);
+                }
+                interpreter.exec("del value");
+            } catch (JepException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    static String getPythonCode(String script) {
+        if (!script.endsWith(".py")) return script;
+
+        StringBuilder importsBuilder = new StringBuilder();
+        StringBuilder codeBuilder = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(script));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = importPattern.matcher(line);
+                if (matcher.matches()) {
+                    importsBuilder.append(line).append("\n");
+                } else {
+                if (!line.trim().isEmpty()) {
+                    codeBuilder.append(line).append("\n");
+                }
                 }
             }
-            interpreter.exec("del value");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        PythonUtils.importOnceToInterpreter(importsBuilder.toString(), interpreter);
+        return codeBuilder.toString();
     }
 }
---
+++ src/main/java/gr/aueb/data_mingler_optimizations/util/PythonUtils.java
@@ -0,0 +1,34 @@
+package gr.aueb.data_mingler_optimizations.util;
+
+import jep.Interpreter;
+import jep.JepException;
+import jep.MainInterpreter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PythonUtils {
+    private static final List<String> importList = new ArrayList<>();
+
+    public static void initialize(String jepPath) {
+        MainInterpreter.setJepLibraryPath(jepPath);
+    }
+
+    public static void importOnceToInterpreter(String imports, Interpreter interpreter) throws JepException {
+        if (!importList.contains(imports)) {
+            interpreter.exec(imports);
+            importList.add(imports);
+        }
+    }
+
+    public static Object getValueFromScript(String script, Interpreter interpreter) throws JepException {
+        String[] lines = script.split("\n");
+        for (int i = 0; i < lines.length; i++) {
+            interpreter.exec(lines[i]);
+            if (i == lines.length - 1) {
+                 return interpreter.getValue(lines[i]);
+            }
+        }
+        return null;
+    }
+}
