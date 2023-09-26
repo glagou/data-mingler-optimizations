3f65cd61fcb239291373f8a903480f41a0e08ed4
sotirangelo <sotiraggelos@gmail.com>
2023-09-26 23:10:12 +0300

    Fix jep invalid syntax issue

--- src/main/java/gr/aueb/data_mingler_optimizations/operator/MapOperator.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/operator/MapOperator.java
@@ -3,9 +3,7 @@ package gr.aueb.data_mingler_optimizations.operator;
 import gr.aueb.data_mingler_optimizations.enumerator.GraphAdditionMethod;
 import gr.aueb.data_mingler_optimizations.util.PythonUtils;
 import gr.aueb.data_mingler_optimizations.util.GraphUtils;
-import jep.Interpreter;
 import jep.JepException;
-import jep.SharedInterpreter;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -16,11 +14,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MapOperator {
-    static final Pattern importPattern = Pattern.compile("^import .*|^from .* import .*");
-
 
     public static void run(String rootNode, String childNode, String script) {
-        String pythonCode = getPythonCode(script).replace('$' + childNode + '$', "value");
+        String pythonCode = script.replace('$' + childNode + '$', "value");
 
         String edge = rootNode + "-" + childNode;
         Set<String> keys = (Set<String>) GraphUtils.getElements(edge);
@@ -42,28 +38,5 @@ public class MapOperator {
         }
     }
 
-    static String getPythonCode(String script) {
-        if (!script.endsWith(".py")) return script;
 
-        StringBuilder importsBuilder = new StringBuilder();
-        StringBuilder codeBuilder = new StringBuilder();
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(script));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                Matcher matcher = importPattern.matcher(line);
-                if (matcher.matches()) {
-                    importsBuilder.append(line).append("\n");
-                } else {
-                if (!line.trim().isEmpty()) {
-                    codeBuilder.append(line).append("\n");
-                }
-                }
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        PythonUtils.importOnceToInterpreter(importsBuilder.toString());
-        return codeBuilder.toString();
-    }
 }
--- src/main/java/gr/aueb/data_mingler_optimizations/util/PythonUtils.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/util/PythonUtils.java
@@ -5,43 +5,71 @@ import jep.JepException;
 import jep.MainInterpreter;
 import jep.SharedInterpreter;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PythonUtils {
     private static final List<String> importList = new ArrayList<>();
-    private static final Interpreter interpreter = new SharedInterpreter();
+    private static final Pattern importPattern = Pattern.compile("^import .*|^from .* import .*");
+    private static final Interpreter interpreter = new SharedInterpreter(); // TODO: place multiple shared interpreters in operators
 
     public static Interpreter getInterpreter() {
         return interpreter;
     }
 
-    public static void importOnceToInterpreter(String imports) throws JepException {
-        if (!importList.contains(imports)) {
-            interpreter.exec(imports);
-            importList.add(imports);
-        }
+    public static Object getValueFromScript(String script) {
+        return executePython(script);
     }
 
-    public static Object getValueFromScript(String script) throws JepException {
-        String[] lines = script.split("\n");
-        for (int i = 0; i < lines.length; i++) {
-            interpreter.exec(lines[i]);
-            if (i == lines.length - 1) {
-                 return interpreter.getValue(lines[i]);
+    public static boolean evalFromScript(String script) {
+        Object obj = executePython(script);
+        if (obj instanceof Boolean) {
+            return (boolean) obj;
+        }
+        throw new RuntimeException("Python script did not return boolean");
+    }
+
+    private static Object executePython(String script) {
+        if (!script.endsWith(".py")) {
+            String[] lines = script.split("\n");
+            for (int i = 0; i < lines.length; i++) {
+                interpreter.exec(lines[i].trim());
+                if (i == lines.length - 1) {
+                    return interpreter.getValue(lines[i].trim());
+                }
+
             }
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(script));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = importPattern.matcher(line);
+                if (matcher.matches() && !importList.contains(line)) {
+                    importList.add(line);
+                    interpreter.exec(line);
+                    continue;
+                }
+                if (!line.trim().isEmpty()) {
+                    String nextLine = reader.readLine();
+                    if (nextLine == null) {
+                        return interpreter.getValue(line);
+                    }
+                    interpreter.exec(line);
+                }
+
+            }
+        } catch (IOException | JepException e) {
+            throw new RuntimeException(e);
         }
         return null;
     }
 
-    public static boolean evalFromScript(String script) throws JepException {
-        String[] lines = script.split("\n");
-        for (int i = 0; i < lines.length; i++) {
-            interpreter.exec(lines[i]);
-            if (i == lines.length - 1) {
-                return interpreter.eval(lines[i]);
-            }
-        }
-        throw new RuntimeException("Something went wrong with the evaluation of the script");
-    }
+
 }
