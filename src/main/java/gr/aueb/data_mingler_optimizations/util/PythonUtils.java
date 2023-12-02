50916c9bff08afca4033edb67e33f4103e6788fd
sotirangelo <sotiraggelos@gmail.com>
2023-12-02 14:10:27 +0200

    Refactor PythonUtils to use Processes

--- src/main/java/gr/aueb/data_mingler_optimizations/util/PythonUtils.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/util/PythonUtils.java
@@ -1,37 +1,40 @@
 package gr.aueb.data_mingler_optimizations.util;
 
 import gr.aueb.data_mingler_optimizations.python.Script;
-import jep.Interpreter;
-import jep.JepException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.*;
 
 public class PythonUtils {
-    public static Object getValueFromScript(Interpreter interpreter, Script pythonCode) {
-        return executePython(interpreter, pythonCode);
+    private static final int numberOfCores = Runtime.getRuntime().availableProcessors();
+    private static final ExecutorService executor = Executors.newFixedThreadPool(numberOfCores);
+
+    public static CompletableFuture<String> executePython(Script pythonCode) {
+        return CompletableFuture.supplyAsync(() -> createPythonProcess(pythonCode), executor);
     }
 
-    public static boolean evalFromScript(Interpreter interpreter, Script pythonCode) {
-        Object obj = executePython(interpreter, pythonCode);
-        if (obj instanceof Boolean) {
-            return (boolean) obj;
-        }
-        throw new RuntimeException("Python script did not return boolean");
-    }
-
-    private static Object executePython(Interpreter interpreter, Script pythonCode) {
-        String[] lines = pythonCode.getScript();
+    private static String createPythonProcess(Script pythonCode) {
+        String code = pythonCode.getScript();
+        ProcessBuilder processBuilder = new ProcessBuilder("python3", "-c", code);
         try {
-            for (int i = 0; i < lines.length - 1; i++) {
-                String line = lines[i];
-                if (!line.trim().isEmpty()) {
-                    interpreter.exec(line);
-                }
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            String lastStoredLine = null;
+            while ((line = reader.readLine()) != null) {
+                lastStoredLine = line;
             }
-            String lastLine = lines[lines.length - 1];
-            return interpreter.getValue(lastLine);
-        } catch (JepException e) {
+            int exitCode = process.waitFor();
+            if (exitCode == 0) {
+                return lastStoredLine;
+            } else {
+                throw new RuntimeException("Python script exited with code " + exitCode);
+            }
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
-
 
 }
