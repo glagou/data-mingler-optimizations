f4e3c4c924fbbebf4060c8ef815cbc5a182b8e9e
sotirangelo <sotiraggelos@gmail.com>
2023-12-02 22:46:36 +0200

    Refactor QueryEvaluation for parallelism

--- src/main/java/gr/aueb/data_mingler_optimizations/QueryEvaluation.java
+++ src/main/java/gr/aueb/data_mingler_optimizations/QueryEvaluation.java
@@ -9,7 +9,6 @@ import gr.aueb.data_mingler_optimizations.exception.LoadEdgesExecutionFailedExce
 import gr.aueb.data_mingler_optimizations.exception.UnableToInitializeDocumentAndXpathException;
 import gr.aueb.data_mingler_optimizations.load.EdgesLoader;
 import gr.aueb.data_mingler_optimizations.load.EnvLoader;
-import gr.aueb.data_mingler_optimizations.util.PythonUtils;
 import gr.aueb.data_mingler_optimizations.util.GraphUtils;
 import gr.aueb.data_mingler_optimizations.util.OperatorUtils;
 import jep.MainInterpreter;
@@ -30,6 +29,8 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 
 /**
@@ -57,18 +58,18 @@ public class QueryEvaluation {
 
     private static final Map<String, List<String>> NODE_TO_CHILDREN_NODES = new HashMap<>();
     private static final Map<String, String> DVM_NODES_TO_LABELS = new HashMap<>();
-    private static final Map<String, String> NODE_TO_TRANSFORMATIONS = new HashMap<>();
-    private static final Map<String, String> THETAS_ON_INTERNAL_NODES = new HashMap<>();
-    private static final Map<String, String> OUTPUTS = new HashMap<>();
+    private static final ConcurrentMap<String, String> NODE_TO_TRANSFORMATIONS = new ConcurrentHashMap<>();
+    private static final ConcurrentMap<String, String> THETAS_ON_INTERNAL_NODES = new ConcurrentHashMap<>();
+    private static final Map<String, Boolean> OUTPUTS = new HashMap<>();
     private static final List<String> LOAD_EDGES_CMD_ARGS = new ArrayList<>();
 
-    private static String queryFilename;
+    private static String queryFilePath;
     private static OutputType outputType;
     private static KeysMode keysMode;
-    private static Document document;
+    private static Document queryDocument;
     private static XPath xpath;
     private static String rootNode;
-    private static List<String> childNodes;
+    private static List<String> branchRootNodes;
 
     private static void validateCmdArguments(String[] args) {
         if (args.length != 3) {
@@ -77,7 +78,7 @@ public class QueryEvaluation {
     }
 
     private static void initializeValuesFromCmdArguments(String[] args) {
-        queryFilename = args[0];
+        queryFilePath = args[0];
         outputType = OutputType.valueOf(args[1].toUpperCase());
         keysMode = KeysMode.valueOf(args[2].toUpperCase());
     }
@@ -85,18 +86,18 @@ public class QueryEvaluation {
     private static void initializeDocumentAndXpath() {
         try {
             DocumentBuilder parser = DocumentBuilderFactory.newInstance().newDocumentBuilder();
-            document = parser.parse(new File(queryFilename));
+            queryDocument = parser.parse(new File(queryFilePath));
         } catch (SAXException | IOException | ParserConfigurationException e) {
-            throw new UnableToInitializeDocumentAndXpathException(queryFilename);
+            throw new UnableToInitializeDocumentAndXpathException(queryFilePath);
         }
         xpath = XPathFactory.newInstance().newXPath();
     }
 
     private static void populateNodeToChildrenNodes() throws XPathExpressionException {
         int rows = 1;
-        while (!xpath.evaluate("/query/node[position()=" + rows + "]", document).trim().isEmpty()) {
-            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", document).trim();
-            String children = xpath.evaluate("/query/node[position()=" + rows + "]/children", document).trim();
+        while (!xpath.evaluate("/query/node[position()=" + rows + "]", queryDocument).trim().isEmpty()) {
+            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", queryDocument).trim();
+            String children = xpath.evaluate("/query/node[position()=" + rows + "]/children", queryDocument).trim();
             String[] child = children.split(",", -1);
             List<String> childrenList = new ArrayList<>();
             if (!children.isEmpty()) {
@@ -109,9 +110,9 @@ public class QueryEvaluation {
 
     private static void populateDvmNodesToLabels() throws XPathExpressionException {
         int rows = 1;
-        while (!xpath.evaluate("/query/node[position()=" + rows + "]", document).trim().isEmpty()) {
-            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", document).trim();
-            String onNode = xpath.evaluate("/query/node[position()=" + rows + "]/onnode", document).trim();
+        while (!xpath.evaluate("/query/node[position()=" + rows + "]", queryDocument).trim().isEmpty()) {
+            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", queryDocument).trim();
+            String onNode = xpath.evaluate("/query/node[position()=" + rows + "]/onnode", queryDocument).trim();
             DVM_NODES_TO_LABELS.put(label, onNode);
             rows++;
         }
@@ -119,9 +120,9 @@ public class QueryEvaluation {
 
     private static void populateNodeToTransformations() throws XPathExpressionException {
         int rows = 1;
-        while (!xpath.evaluate("/query/node[position()=" + rows + "]", document).trim().isEmpty()) {
-            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", document).trim();
-            String transf = xpath.evaluate("/query/node[position()=" + rows + "]/transformations", document).trim();
+        while (!xpath.evaluate("/query/node[position()=" + rows + "]", queryDocument).trim().isEmpty()) {
+            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", queryDocument).trim();
+            String transf = xpath.evaluate("/query/node[position()=" + rows + "]/transformations", queryDocument).trim();
             NODE_TO_TRANSFORMATIONS.put(label, transf);
             rows++;
         }
@@ -129,9 +130,9 @@ public class QueryEvaluation {
 
     private static void populateThetasOnInternalNodes() throws XPathExpressionException {
         int rows = 1;
-        while (!xpath.evaluate("/query/node[position()=" + rows + "]", document).trim().isEmpty()) {
-            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", document).trim();
-            String theta = xpath.evaluate("/query/node[position()=" + rows + "]/theta", document).trim();
+        while (!xpath.evaluate("/query/node[position()=" + rows + "]", queryDocument).trim().isEmpty()) {
+            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", queryDocument).trim();
+            String theta = xpath.evaluate("/query/node[position()=" + rows + "]/theta", queryDocument).trim();
             THETAS_ON_INTERNAL_NODES.put(label, theta);
             rows++;
         }
@@ -139,17 +140,17 @@ public class QueryEvaluation {
 
     private static void populateOutputs() throws XPathExpressionException {
         int rows = 1;
-        while (!xpath.evaluate("/query/node[position()=" + rows + "]", document).trim().isEmpty()) {
-            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", document).trim();
-            String output = xpath.evaluate("/query/node[position()=" + rows + "]/output", document).trim();
-            OUTPUTS.put(label, output);
+        while (!xpath.evaluate("/query/node[position()=" + rows + "]", queryDocument).trim().isEmpty()) {
+            String label = xpath.evaluate("/query/node[position()=" + rows + "]/label", queryDocument).trim();
+            String output = xpath.evaluate("/query/node[position()=" + rows + "]/output", queryDocument).trim();
+            OUTPUTS.put(label, output.equalsIgnoreCase(Output.YES.getValue()));
             rows++;
         }
     }
 
     private static void initializeRootNodeAndChildNodes() throws XPathExpressionException {
-        rootNode = xpath.evaluate("/query/rootnode", document).trim();
-        childNodes = NODE_TO_CHILDREN_NODES.get(rootNode);
+        rootNode = xpath.evaluate("/query/rootnode", queryDocument).trim();
+        branchRootNodes = NODE_TO_CHILDREN_NODES.get(rootNode);
     }
 
     private static void materializeEdge(String rootNode, List<String> childNodes) {
@@ -160,7 +161,7 @@ public class QueryEvaluation {
                     "(" + childNodeDVM + ")");
 
             String key = rootNode + "-" + childNode;
-            if (GraphUtils.getElements(key) == null || GraphUtils.getElements(key).size() == 0) {
+            if (GraphUtils.getElements(key) == null || GraphUtils.getElements(key).isEmpty()) {
                 System.out.println("Loading edge from data source: " + key);
                 LOAD_EDGES_CMD_ARGS.add(rootNodeDVM);
                 LOAD_EDGES_CMD_ARGS.add(childNodeDVM);
@@ -171,7 +172,7 @@ public class QueryEvaluation {
             }
 
             List<String> childrenOfChildNode = NODE_TO_CHILDREN_NODES.get(childNode);
-            if (childrenOfChildNode.size() != 0) {
+            if (!childrenOfChildNode.isEmpty()) {
                 materializeEdge(childNode, childrenOfChildNode);
             }
         });
@@ -190,51 +191,52 @@ public class QueryEvaluation {
     private static void evaluateChild(String rootNode, String childNode) {
         System.out.println("------ Evaluating Node: " + childNode + "(root:" + rootNode + ")");
         List<String> childrenOfChildNode = NODE_TO_CHILDREN_NODES.get(childNode);
-        if (childrenOfChildNode.size() > 0) {
-            childrenOfChildNode.forEach(childOfChildNode -> {
-                evaluateChild(childNode, childOfChildNode);
-                OperatorUtils.executeTransformationOnEdge(childNode, childOfChildNode);
-            });
+        if (childrenOfChildNode.isEmpty()) return;
 
-            StringBuilder allChildNodes = new StringBuilder();
-            childrenOfChildNode.forEach(childOfChildNodeIter -> {
-                allChildNodes.append(childOfChildNodeIter);
-                allChildNodes.append(StringConstant.COMMA.getValue());
-            });
+        childrenOfChildNode.forEach(childOfChildNode -> {
+            evaluateChild(childNode, childOfChildNode);
+            OperatorUtils.executeTransformationOnEdge(childNode, childOfChildNode);
+        });
 
-            StringBuilder outputChildNodes = new StringBuilder();
-            childrenOfChildNode.forEach(childOfChildNodeIter -> {
-                if (OUTPUTS.get(childOfChildNodeIter).equalsIgnoreCase(Output.YES.getValue())) {
-                    outputChildNodes.append(childOfChildNodeIter);
-                    outputChildNodes.append(StringConstant.COMMA.getValue());
-                }
+        StringBuilder allChildNodes = new StringBuilder();
+        childrenOfChildNode.forEach(childOfChildNodeIter -> {
+            allChildNodes.append(childOfChildNodeIter);
+            allChildNodes.append(StringConstant.COMMA.getValue());
+        });
 
-            });
+        StringBuilder outputChildNodes = new StringBuilder();
+        childrenOfChildNode.forEach(childOfChildNodeIter -> {
+            if (OUTPUTS.get(childOfChildNodeIter)) {
+                outputChildNodes.append(childOfChildNodeIter);
+                outputChildNodes.append(StringConstant.COMMA.getValue());
+            }
 
-            String theta = THETAS_ON_INTERNAL_NODES.get(childNode);
-            String childOfChildNode = StringConstant.CHILD_OF_PREFIX.getValue().concat(childNode);
-            OperatorUtils.executeThetaCombine(childNode, childOfChildNode, allChildNodes.toString(),
-                    outputChildNodes.toString(), theta);
-            OperatorUtils.executeRollupEdges(rootNode, childNode, childOfChildNode);
-        }
+        });
+
+        String theta = THETAS_ON_INTERNAL_NODES.get(childNode);
+        String childOfChildNode = StringConstant.CHILD_OF_PREFIX.getValue().concat(childNode);
+        OperatorUtils.executeThetaCombine(childNode, childOfChildNode, allChildNodes.toString(),
+                outputChildNodes.toString(), theta);
+        OperatorUtils.executeRollupEdges(rootNode, childNode, childOfChildNode);
     }
 
     private static void evaluateChildAndExecuteTransformations() {
         // Create a parallel stream to evaluate and transform child nodes concurrently
-        childNodes
-                .forEach(childNode -> {
-            evaluateChild(rootNode, childNode);
-            System.out.println("Finished evaluating child: " + childNode);
-            OperatorUtils.executeTransformationOnEdge(rootNode, childNode);
-            System.out.println("Finished transforming child: " + childNode);
-        });
+        branchRootNodes
+            .parallelStream()
+            .forEach(branchRootNode -> {
+                evaluateChild(rootNode, branchRootNode);
+                System.out.println("Finished evaluating child: " + branchRootNode);
+                OperatorUtils.executeTransformationOnEdge(rootNode, branchRootNode);
+                System.out.println("Finished transforming child: " + branchRootNode);
+            });
     }
 
 
     private static List<String> initializeOutputChildNodes() {
         List<String> outputChildNodes = new ArrayList<>();
-        childNodes.forEach(childNode -> {
-            if (OUTPUTS.get(childNode).equalsIgnoreCase(Output.YES.getValue())) {
+        branchRootNodes.forEach(childNode -> {
+            if (OUTPUTS.get(childNode)) {
                 outputChildNodes.add(childNode);
             }
         });
@@ -246,7 +248,7 @@ public class QueryEvaluation {
         Date date = new Date();
         String dt = formatter.format(date);
         String outFilename = rootNode + "_" + dt + ".csv";
-        if (queryFilename.equals("tempQuery.qdvm.xml")) {
+        if (queryFilePath.equals("tempQuery.qdvm.xml")) {
             outFilename = "tempQuery.csv";
         }
         FileWriter outFile = new FileWriter(outFilename);
@@ -298,7 +300,7 @@ public class QueryEvaluation {
         populateThetasOnInternalNodes();
         populateOutputs();
         initializeRootNodeAndChildNodes();
-        materializeEdge(rootNode, childNodes);
+        materializeEdge(rootNode, branchRootNodes);
         loadEdges();
         System.out.println("Finished loading edges");
         evaluateChildAndExecuteTransformations();
